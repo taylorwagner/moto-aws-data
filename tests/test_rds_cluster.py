3092b87c3953a602f2b25975a016a6d3169e09fc
Taylor Wagner <taylor.wagner@slalom.com>
2022-12-20 09:18:18 -0600

    create db cluster snapshot

--- tests/test_rds_cluster.py
+++ tests/test_rds_cluster.py
@@ -30,3 +30,22 @@ class TestRDSCluster:
             cluster_details = res['DBClusters'][0]
 
             assert cluster_details['DBClusterIdentifier'] == "my-cluster"
+
+
+    def test_create_db_cluster_snapshot(self, rds_client):
+        """Test creation of mock RDS cluster snapshot"""
+
+        with create_db_cluster(rds_client):
+
+            res = rds_client.create_db_cluster_snapshot(
+                DBClusterSnapshotIdentifier="my-cluster-snap",
+                DBClusterIdentifier="my-cluster"
+            )
+            describe_snapshot = rds_client.describe_db_cluster_snapshots(
+                DBClusterSnapshotIdentifier="my-cluster-snap",
+                DBClusterIdentifier="my-cluster"
+            )
+            
+            assert res['DBClusterSnapshot']['DBClusterSnapshotIdentifier'] == 'my-cluster-snap'
+            assert res['DBClusterSnapshot']['SnapshotCreateTime'] != None
+            assert describe_snapshot['DBClusterSnapshots'][0]['DBClusterIdentifier'] == 'my-cluster'
