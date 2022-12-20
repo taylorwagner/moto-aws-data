a0df5462502c18cfa63b6765a3ecf1f85c3d831e
Taylor Wagner <taylor.wagner@slalom.com>
2022-12-20 00:29:00 -0600

    remove tagging tests -- increase reusability with TABLE_NAME variable

--- tests/test_dynamodb.py
+++ tests/test_dynamodb.py
@@ -32,27 +32,26 @@ def create_table(dynamodb_client):
         ProvisionedThroughput={
             'ReadCapacityUnits': 10,
             'WriteCapacityUnits': 10
-        },
-        Tags=[
-            {"Key": "TestTag", "Value": "TestValue"},
-            {"Key": "TestTag2", "Value": "TestValue2"},
-        ]
+        }
     )
     yield
 
+
 class TestDynamoDB:
     """Test CRUD operations on mock DynamoDB table"""
+
+    TABLE_NAME = "my-test-table"
 
     def test_create_table(self, dynamodb_client):
         """Test creation of 'my-test-table' DynamoDB table"""
 
         with create_table(dynamodb_client):
 
-            res = dynamodb_client.describe_table(TableName="my-test-table")
+            res = dynamodb_client.describe_table(TableName=self.TABLE_NAME)
             res2 = dynamodb_client.list_tables()
 
-            assert res['Table']['TableName'] == "my-test-table"
-            assert res2['TableNames'] == ["my-test-table"]
+            assert res['Table']['TableName'] == self.TABLE_NAME
+            assert res2['TableNames'] == [self.TABLE_NAME]
 
 
     def test_put_item(self, dynamodb_client):
@@ -61,7 +60,7 @@ class TestDynamoDB:
         with create_table(dynamodb_client):
 
             add_item = dynamodb_client.put_item(
-                TableName="my-test-table",
+                TableName=self.TABLE_NAME,
                 Item={
                     "attribute1": {"S": "attribute1_value"},
                     "attribute2": {"S": "attribute2_value"},
@@ -69,7 +68,7 @@ class TestDynamoDB:
             )
 
             res = dynamodb_client.get_item(
-                TableName="my-test-table",
+                TableName=self.TABLE_NAME,
                 Key={
                     "attribute1": {"S": "attribute1_value"},
                     "attribute2": {"S": "attribute2_value"},
@@ -81,6 +80,38 @@ class TestDynamoDB:
             assert len(res['Item']) == 2
 
 
+    # def test_update_item(self, dynamodb_client):
+    #     """Test updating an item to 'my-test-table' DynamoDB table"""
+
+    #     with create_table(dynamodb_client):
+
+    #         ## Add an item to update
+    #         dynamodb_client.put_item(
+    #             TableName=self.TABLE_NAME,
+    #             Item={
+    #                 "attribute1": {"S": "attribute1_value"},
+    #                 "attribute2": {"S": "attribute2_value"},
+    #                 "attribute3": {"S": "attribute3_value"}
+    #             },
+    #         )
+
+    #         ## Update previously added item
+    #         dynamodb_client.update_item(
+    #             TableName=self.TABLE_NAME,
+    #             Key={"attribute1": "attribute1_value", "attribute2": "attribute2_value", "attribute3": "attribute3_value"},
+    #             UpdateExpression="REMOVE attribute3",
+    #             ReturnValues="UPDATED_NEW"
+    #         )
+
+    #         res = dynamodb_client.get_item(
+    #             TableName=self.TABLE_NAME,
+    #             Key={
+    #                 "attribute1": {"S": "attribute1_value"}
+    #             },
+    #         )
+    #         print(res)
+
+
     def test_delete_item(self, dynamodb_client):
         """Test deleting an item from 'my-test-table' DynamoDB table"""
 
@@ -88,7 +119,7 @@ class TestDynamoDB:
 
             ## Add an item to delete
             dynamodb_client.put_item(
-                TableName="my-test-table",
+                TableName=self.TABLE_NAME,
                 Item={
                     "attribute1": {"S": "attribute1_value"},
                     "attribute2": {"S": "attribute2_value"},
@@ -97,7 +128,7 @@ class TestDynamoDB:
 
             ## Delete previously added item
             dynamodb_client.delete_item(
-                TableName="my-test-table",
+                TableName=self.TABLE_NAME,
                 Key={
                     "attribute1": {"S": "attribute1_value"},
                     "attribute2": {"S": "attribute2_value"},
@@ -105,7 +136,7 @@ class TestDynamoDB:
             )
 
             res = dynamodb_client.get_item(
-                TableName="my-test-table",
+                TableName=self.TABLE_NAME,
                 Key={
                     "attribute1": {"S": "attribute1_value"},
                     "attribute2": {"S": "attribute2_value"},
@@ -116,71 +147,12 @@ class TestDynamoDB:
                 assert res['ResponseMetadata']['HTTPStatusCode'] == 200
 
 
-    def test_list_table_tags(self, dynamodb_client):
-        """Test listing the tags on 'my-test-table' DynamoDB table"""
-
-        with create_table(dynamodb_client):
-
-            res = dynamodb_client.describe_table(TableName="my-test-table")
-            table_arn = res['Table']['TableArn']
-            list_tags = dynamodb_client.list_tags_of_resource(ResourceArn=table_arn)
-
-            assert table_arn == "arn:aws:dynamodb:us-east-1:123456789012:table/my-test-table"
-            assert list_tags['Tags'][0] == {'Key': 'TestTag', 'Value': 'TestValue'}, {'Key': 'TestTag2', 'Value': 'TestValue2'}
-            assert len(list_tags['Tags']) == 2
-
-
-    def test_add_tags(self, dynamodb_client):
-        """Test adding tags to 'my-test-table' DynamoDB table"""
-
-        with create_table(dynamodb_client):
-
-            res = dynamodb_client.describe_table(TableName="my-test-table")
-            table_arn = res['Table']['TableArn']
-
-            dynamodb_client.tag_resource(
-                ResourceArn=table_arn,
-                Tags=[
-                    {"Key": "NewTag", "Value": "NewValue"},
-                    {"Key": "NewestTag", "Value": "NewestValue"},
-                ]
-            )
-
-            list_tags = dynamodb_client.list_tags_of_resource(ResourceArn=table_arn)
-
-            assert list_tags['Tags'][2] == {'Key': 'NewTag', 'Value': 'NewValue'}
-            assert list_tags['Tags'][3] == {'Key': 'NewestTag', 'Value': 'NewestValue'}
-            assert len(list_tags['Tags']) == 4
-
-
-    def test_remove_tags(self, dynamodb_client):
-        """Test removing tags from 'my-test-table' DynamoDB table"""
-
-        with create_table(dynamodb_client):
-
-            res = dynamodb_client.describe_table(TableName="my-test-table")
-            table_arn = res['Table']['TableArn']
-
-            dynamodb_client.untag_resource(
-                ResourceArn=table_arn,
-                TagKeys=[
-                    'TestTag',
-                    'TestTag2'
-                ]
-            )
-
-            list_tags = dynamodb_client.list_tags_of_resource(ResourceArn=table_arn)
-
-            assert list_tags['Tags'] == []
-            assert len(list_tags['Tags']) == 0
-
-
     def test_delete_table(self, dynamodb_client):
         """Test deletion of 'my-test-table' DynamoDB table"""
 
         with create_table(dynamodb_client):
 
-            dynamodb_client.delete_table(TableName="my-test-table")
+            dynamodb_client.delete_table(TableName=self.TABLE_NAME)
             res = dynamodb_client.list_tables()
 
-            assert res['TableNames'] != ["my-test-table"]
+            assert res['TableNames'] != [self.TABLE_NAME]
