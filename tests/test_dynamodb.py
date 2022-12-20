3ab4e7b06e8192af30de513973835359064e1374
Taylor Wagner <taylor.wagner@slalom.com>
2022-12-19 22:45:13 -0600

    uniform spacing

--- tests/test_dynamodb.py
+++ tests/test_dynamodb.py
@@ -50,8 +50,10 @@ class TestDynamoDB:
 
             res = dynamodb_client.describe_table(TableName="my-test-table")
             res2 = dynamodb_client.list_tables()
+
             assert res['Table']['TableName'] == "my-test-table"
             assert res2['TableNames'] == ["my-test-table"]
+
 
     def test_put_item(self, dynamodb_client):
         """Test adding an item to 'my-test-table' DynamoDB table"""
@@ -78,6 +80,7 @@ class TestDynamoDB:
             assert res['Item']['attribute1'] == {"S": "attribute1_value"}
             assert len(res['Item']) == 2
 
+
     def test_list_table_tags(self, dynamodb_client):
         """Test listing the tags on 'my-test-table' DynamoDB table"""
 
@@ -90,6 +93,7 @@ class TestDynamoDB:
             assert table_arn == "arn:aws:dynamodb:us-east-1:123456789012:table/my-test-table"
             assert list_tags['Tags'][0] == {'Key': 'TestTag', 'Value': 'TestValue'}, {'Key': 'TestTag2', 'Value': 'TestValue2'}
             assert len(list_tags['Tags']) == 2
+
 
     def test_add_tags(self, dynamodb_client):
         """Test adding tags to 'my-test-table' DynamoDB table"""
@@ -113,6 +117,7 @@ class TestDynamoDB:
             assert list_tags['Tags'][3] == {'Key': 'NewestTag', 'Value': 'NewestValue'}
             assert len(list_tags['Tags']) == 4
 
+
     def test_remove_tags(self, dynamodb_client):
         """Test removing tags from 'my-test-table' DynamoDB table"""
 
@@ -134,6 +139,7 @@ class TestDynamoDB:
             assert list_tags['Tags'] == []
             assert len(list_tags['Tags']) == 0
 
+
     def test_delete_table(self, dynamodb_client):
         """Test deletion of 'my-test-table' DynamoDB table"""
 
@@ -141,4 +147,5 @@ class TestDynamoDB:
 
             dynamodb_client.delete_table(TableName="my-test-table")
             res = dynamodb_client.list_tables()
+            
             assert res['TableNames'] != ["my-test-table"]
